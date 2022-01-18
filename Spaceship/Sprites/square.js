289c11384cbfbbacaa5a37d45391bbd161a4fdaf
Hassaan Muhammad <P2645492@my365.dmu.ac.uk>
2022-01-18 12:31:39 +0000

    commit

--- Spaceship/Sprites/square.js
+++ Spaceship/Sprites/square.js
@@ -9,7 +9,7 @@ function Square() {
     //create a public property called X (note caps!)
     Object.defineProperty(this, 'X',
         {
-            //getter function
+            //getter functions
             get: function () {
                 //return the value of x (lower case)
                 return x;
